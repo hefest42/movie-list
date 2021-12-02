91383f0ef533887bfbe2ba9563c7ff63015a8e1c
Nemanja Vidovic <hefest93@yahoo.com>
2021-12-02 14:13:53 +0100

    Added search bar to the left column

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import LeftColumnTitle from "./LeftColumnTitle";
+import LeftColumnSearch from "./LeftColumnSearch";
 
 const AppContainer = (props) => {
     return (
         <div className="container">
             <div className="container-left">
                 <LeftColumnTitle />
+                <LeftColumnSearch />
             </div>
             <div className="container-right">test2</div>
         </div>
---
+++ src/components/LeftColumnSearch.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LeftColumnSearch = () => {
+    return (
+        <form>
+            <input type="text" name="" id="" />
+            <div>
+                <button>Search</button>
+                <button>Reset</button>
+            </div>
+        </form>
+    );
+};
+
+export default LeftColumnSearch;

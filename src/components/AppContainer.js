578437c0b276af2f86caef529445209b55f62a58
Nemanja Vidovic <hefest93@yahoo.com>
2021-12-02 13:39:37 +0100

    updated layout

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const AppContainer = (props) => {
-  return <div>test</div>;
+    return (
+        <div className="container">
+            <div className="container-left">test</div>
+            <div className="container-right"></div>
+        </div>
+    );
 };
 
 export default AppContainer;

1057d5b5a0ee933493ec5ab2b32db829f2a5e7de
Nemanja Vidovic <hefest93@yahoo.com>
2021-12-02 14:57:39 +0100

    initial commit

---
+++ src/components/LeftColumnInputs.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LeftColumnInputs = () => {
+    return (
+        <div className="left-column__inputs">
+            <label htmlFor="">test</label>
+            <input type="checkbox" />
+        </div>
+    );
+};
+
+export default LeftColumnInputs;

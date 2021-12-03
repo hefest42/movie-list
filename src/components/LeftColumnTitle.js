ad77f0f7d2ec24c62309e2f239c99228006ff177
Nemanja Vidovic <hefest93@yahoo.com>
2021-12-03 13:17:39 +0100

    put "movies" in a span

--- src/components/LeftColumnTitle.js
+++ src/components/LeftColumnTitle.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const LeftColumnTitle = () => {
-    return <h2 className="left-column__title">Movies Watched in 2021</h2>;
+    return (
+        <h2 className="left-column__title">
+            <span>Movies</span> Watched in 2021
+        </h2>
+    );
 };
 
 export default LeftColumnTitle;

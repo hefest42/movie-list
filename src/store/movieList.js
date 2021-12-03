67ce35ad076e522a4079f7c2940891c257826135
Nemanja Vidovic <hefest93@yahoo.com>
2021-12-03 15:03:48 +0100

    added movies to the list

--- src/store/movieList.js
+++ src/store/movieList.js
@@ -1,13 +1,111 @@
-export default const movieList = [
+//prettier-ignore
+export const movieList = [
     {
         id: "movie1",
         name: "Rising Arizona",
+        releaseYear: "1987",
+        runtime: "1h34m",
+        summary:"When a childless couple of an ex-con and an ex-cop decide to help themselves to one of another family's quintuplets, their lives become more complicated than they anticipated.",
+        director: ["Joel Coen", "Ethan Coen"],
+        writers: ["Joel Coen", "Ethan Coen"],
+        actors: ["Nicolas Cage","Holly Hunter","Trey Wilson","John Goodman","William Forsythe","Sam McMurray","Frances McDormand",],
+        genre: ["comedy", "crime"],
+        poster: "https://m.media-amazon.com/images/M/MV5BOGUwZTEwZTYtZmQ0OS00NmM1LWE2ZTQtNjZiYmE4ZmUxMjk1XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
+        links: ["https://www.imdb.com/title/tt0093822/", "https://www.rottentomatoes.com/m/raising_arizona"],
+    },
+    {
+        id: "movie2",
+        name: "Troy",
+        releaseYear: "2004",
+        runtime: "2h 43m",
+        summary:"An adaptation of Homer's great epic, the film follows the assault on Troy by the united Greek forces and chronicles the fates of the men involved.",
+        director: ["Wolfgang Petersen"],
+        writers: ["Homer", "David Benioff"],
+        actors: ["Brad Pitt", "Eric Bana", "Orlando Bloom", "Julian Glover", "Brian Cox", "Nathan Jones", "Adoni Maropis", "Diane Kruger", "Sean Bean", "Garrett Hedlund"],
+        genre: ["Drama", "History"],
+        poster: "https://www.google.com/search?q=troy&rlz=1C1GCEA_enRS982RS982&sxsrf=AOaemvJGluvsC-l10Bj3UVXBr3A5P_HsKw:1638539325849&tbm=isch&source=iu&ictx=1&fir=L467Cuw1gJr5KM%252CMuimAWKOrdxauM%252C%252Fm%252F01vw8k%253B2EcXFHHwnC0jjM%252CerWJWK99oe78fM%252C_%253BQfTCrxDxu-8pRM%252C48HO_ko48qGUpM%252C_%253BQD_gx5jDkh2rJM%252CC5aCWaXdPRsQCM%252C_%253BGfWVY29oJrrERM%252CQbhuRH9LHqJPFM%252C_%253B0TwwHP9O1hfJeM%252CecQ2xOnISafubM%252C_%253B1P6Ga4qM-eMg-M%252C3wtzHnscBIvSgM%252C_%253BxkUmfJ8u_yiBPM%252CeNJzDIZ4rpMiNM%252C_&vet=1&usg=AI4_-kTp2oPEiYYLjSHBZvZlG0X4s59i3w&sa=X&ved=2ahUKEwirp97T4sf0AhX1g_0HHWkeCo8Q_B16BAhgEAE#imgrc=2EcXFHHwnC0jjM",
+        links: ["https://www.imdb.com/title/tt0332452/", "https://www.rottentomatoes.com/m/troy"],
+    },
+    {
+        id: "movie3",
+        name: "The Big Lebowski",
+        releaseYear: "1998",
+        runtime: "1h 57m",
+        summary:"Ultimate L.A. slacker Jeff 'The Dude' Lebowski, mistaken for a millionaire of the same name, seeks restitution for a rug ruined by debt collectors, enlisting his bowling buddies for help while trying to find the millionaire's missing wife.",
+        director: ["Joel Coen", "Ethan Coen"],
+        writers:["Joel Coen", "Ethan Coen"],
+        actors: ["Jeff Bridges", "John Goodman", "Steve Buscemi", "Julianne Moor", "David Huddleston", "Philip Seymour Hoffman"],
+        genre: ["Comedy", "Crime", "Sport"],
+        links: {
+            imdb: "https://www.imdb.com/title/tt0118715/",
+            rottenTomatoes: "https://www.rottentomatoes.com/m/the_big_lebowski",
+            poster: "https://m.media-amazon.com/images/M/MV5BMTQ0NjUzMDMyOF5BMl5BanBnXkFtZTgwODA1OTU0MDE@._V1_.jpg"
+        },
+    },
+    {
+        id: "movie4",
+        name: "",
         releaseYear: "",
-        director: "",
-        actors: "",
-        genre: "",
-        poster: "",
-        links: ""
+        runtime: "",
+        summary:"",
+        director: [],
+        writers:[],
+        actors: [],
+        genre: [],
+        links: {
+            imdb: "",
+            rottenTomatoes: "",
+            poster: ""
+        },
+    },
+    {
+        id: "movie2",
+        name: "",
+        releaseYear: "",
+        runtime: "",
+        summary:"",
+        director: [],
+        writers:[],
+        actors: [],
+        genre: [],
+        links: {
+            imdb: "",
+            rottenTomatoes: "",
+            poster: ""
+        },
+    },
+    {
+        id: "movie2",
+        name: "",
+        releaseYear: "",
+        runtime: "",
+        summary:"",
+        director: [],
+        writers:[],
+        actors: [],
+        genre: [],
+        links: {
+            imdb: "",
+            rottenTomatoes: "",
+            poster: ""
+        },
+    },
+    {
+        id: "movie2",
+        name: "",
+        releaseYear: "",
+        runtime: "",
+        summary:"",
+        director: [],
+        writers:[],
+        actors: [],
+        genre: [],
+        links: {
+            imdb: "",
+            rottenTomatoes: "",
+            poster: ""
+        },
     },
 
-]
+    
+];

a2eb28bf5adc8ad390b95f12804849411cd218e0
Nemanja Vidovic <hefest93@yahoo.com>
2021-12-06 15:16:03 +0100

    added a movie title

--- src/components/MoviePoster.js
+++ src/components/MoviePoster.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-import { movieList } from "../store/movieList";
-
 const MoviePoster = () => {
     return (
-        <div className="poster-container">
+        <div className="poster-container ">
             <img
+                className=""
                 src="https://m.media-amazon.com/images/M/MV5BMTUyMjU1OTUwM15BMl5BanBnXkFtZTgwMDg1NDQ2MjI@._V1_FMjpg_UX1000_.jpg"
                 alt="https://m.media-amazon.com/images/M/MV5BMTUyMjU1OTUwM15BMl5BanBnXkFtZTgwMDg1NDQ2MjI@._V1_FMjpg_UX1000_.jpg"
             ></img>
+            <div className="poster-container-title">Wind River</div>
         </div>
     );
 };

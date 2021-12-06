4f9129bfda4e36f684c0c8af4559de259eda6ceb
Nemanja Vidovic <hefest93@yahoo.com>
2021-12-06 16:07:05 +0100

    added movie poster links

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -24,11 +24,11 @@ const AppContainer = (props) => {
             </div>
 
             <div className="container-right">
-                <MoviePoster />
-                <MoviePoster />
-                <MoviePoster />
-                <MoviePoster />
-                <MoviePoster />
+                <MoviePoster posterLink="https://m.media-amazon.com/images/M/MV5BOGUwZTEwZTYtZmQ0OS00NmM1LWE2ZTQtNjZiYmE4ZmUxMjk1XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg" />
+                <MoviePoster posterLink="https://m.media-amazon.com/images/M/MV5BMTUyMjU1OTUwM15BMl5BanBnXkFtZTgwMDg1NDQ2MjI@._V1_FMjpg_UX1000_.jpg" />
+                <MoviePoster posterLink="https://m.media-amazon.com/images/M/MV5BMTQ0NjUzMDMyOF5BMl5BanBnXkFtZTgwODA1OTU0MDE@._V1_.jpg" />
+                <MoviePoster posterLink="https://m.media-amazon.com/images/M/MV5BMDZjZWE0ZjktZjBlOS00YmFiLWFlYjctY2IwZmUxMzQyZjUyXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg" />
+                <MoviePoster posterLink="https://m.media-amazon.com/images/M/MV5BOWUzYzBjNjAtN2RlNC00YWE5LThlMDEtMjhlOTQwYTY4NTlmXkEyXkFqcGdeQXVyMTI5Nzk4NDQ4._V1_FMjpg_UX1000_.jpg" />
             </div>
         </div>
     );

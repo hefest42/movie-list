f98b896f94874e0a78cc64de1c79225b26b6527d
Nemanja Vidovic <hefest93@yahoo.com>
2021-12-06 14:57:59 +0100

    Update MoviePoster.js

--- src/components/MoviePoster.js
+++ src/components/MoviePoster.js
@@ -3,7 +3,14 @@ import React from "react";
 import { movieList } from "../store/movieList";
 
 const MoviePoster = () => {
-    return <div>{movieList[0].links.poster}</div>;
+    return (
+        <div className="poster-container">
+            <img
+                src="https://m.media-amazon.com/images/M/MV5BMTUyMjU1OTUwM15BMl5BanBnXkFtZTgwMDg1NDQ2MjI@._V1_FMjpg_UX1000_.jpg"
+                alt="https://m.media-amazon.com/images/M/MV5BMTUyMjU1OTUwM15BMl5BanBnXkFtZTgwMDg1NDQ2MjI@._V1_FMjpg_UX1000_.jpg"
+            ></img>
+        </div>
+    );
 };
 
 export default MoviePoster;

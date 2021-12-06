48b70ab7e4a39d67c15a91c23af74fe89fad4cfa
Nemanja Vidovic <hefest93@yahoo.com>
2021-12-06 14:58:31 +0100

    changed the "class" to proper react syntax "className"

--- src/components/LeftColumnTheme.js
+++ src/components/LeftColumnTheme.js
@@ -4,7 +4,7 @@ const LeftColumnTheme = ({ changeMode }) => {
     return (
         <div className="left-column__theme">
             <div className="theme-title">Light/Dark</div>
-            <input id="s1d" type="checkbox" class="switch" onClick={changeMode} />
+            <input id="s1d" type="checkbox" className="switch" onClick={changeMode} />
         </div>
     );
 };

d27de7b6ad537cc709e790ac8cea6146b0c8bd1d
Nemanja Vidovic <hefest93@yahoo.com>
2021-12-07 13:04:54 +0100

    mapped over unique genres

--- src/components/LeftColumnGenres.js
+++ src/components/LeftColumnGenres.js
@@ -2,15 +2,12 @@ import React from "react";
 
 import LabelInput from "./UI/LabelInput";
 
-const LeftColumnInputs = () => {
+const LeftColumnInputs = ({ genres }) => {
     return (
         <div className="left-column__inputs">
-            <LabelInput name="test" title="Horror" />
-            <LabelInput name="test" title="Horror" />
-            <LabelInput name="test" title="Horror" />
-            <LabelInput name="test" title="Horror" />
-            <LabelInput name="test" title="Horror" />
-            <LabelInput name="test" title="Horror" />
+            {genres.map((genre, i) => (
+                <LabelInput key={i} name={genre} title={genre} />
+            ))}
         </div>
     );
 };

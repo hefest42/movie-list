6a3c69586944956cb792c0d0abce93ade10889fa
Nemanja Vidovic <hefest93@yahoo.com>
2021-12-07 16:23:05 +0100

    changed the key prop

--- src/components/LeftColumnGenres.js
+++ src/components/LeftColumnGenres.js
@@ -5,8 +5,8 @@ import LabelInput from "./UI/LabelInput";
 const LeftColumnInputs = ({ genres }) => {
     return (
         <div className="left-column__inputs">
-            {genres.map((genre, i) => (
-                <LabelInput key={i} name={genre} title={genre} />
+            {genres.map((genre) => (
+                <LabelInput key={genre} name={genre} title={genre} />
             ))}
         </div>
     );

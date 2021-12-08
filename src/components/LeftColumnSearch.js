fb05ea95eff3e3469bedea4d161b4cca607d7772
Nemanja Vidovic <hefest93@yahoo.com>
2021-12-08 14:24:41 +0100

    added a button to reset search

--- src/components/LeftColumnSearch.js
+++ src/components/LeftColumnSearch.js
@@ -1,14 +1,17 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 
 import { useDispatch } from "react-redux";
 import { setSearchTerm, filterMoviesWithSearchTerm } from "../store/movieSearch-slice";
 
 const LeftColumnSearch = () => {
+    const [showResetButton, setShowResetButton] = useState(false);
     const dispatch = useDispatch();
     const searchRef = useRef();
 
     const searchFormHandler = (e) => {
         e.preventDefault();
+
+        setShowResetButton(true);
 
         const searchTermValue = searchRef.current.value;
         dispatch(setSearchTerm(searchTermValue));
@@ -17,11 +20,16 @@ const LeftColumnSearch = () => {
         searchRef.current.value = "";
     };
 
+    const resetButtonHandler = () => {
+        setShowResetButton(false);
+    };
+
     return (
         <form className="left-column__search" onSubmit={searchFormHandler}>
             <input type="text" placeholder="Search..." ref={searchRef} />
             <div>
                 <button>Search</button>
+                {showResetButton && <button onClick={resetButtonHandler}>Reset Search</button>}
                 {/* <button>Reset</button> */}
             </div>
         </form>

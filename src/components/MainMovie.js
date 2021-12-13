69c34afc6f5b7f863285f2761e49ba7cbc3dc81a
Nemanja Vidovic <hefest93@yahoo.com>
2021-12-13 19:30:08 +0100

    getting params from the link

--- src/components/MainMovie.js
+++ src/components/MainMovie.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+import { useSelector } from "react-redux";
+
+import { useParams } from "react-router-dom";
+
 const MainMovie = () => {
-    return <div>TEST</div>;
+    const params = useParams();
+    const movies = useSelector((state) => state.movieSearch.movies);
+
+    const [mainMovie] = movies.filter(
+        (movie) =>
+            movie.name.replace(/[^a-z\d\s]+/gi, "").toLowerCase() ===
+            params.movieName.split("-").join(" ").toLowerCase()
+    );
+
+    console.log(mainMovie);
+
+    return <div>{mainMovie.name}</div>;
 };
 
 export default MainMovie;
